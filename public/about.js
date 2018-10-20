a4657d5422805cf5dcaeb98619ce3a5fcdf408dd
PV <pvmoorthi_jobs@yahoo.com>
2018-10-20 14:18:37 -0700

    Add files via upload

---
+++ public/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = (props) => (
+   <h1>About Page com</h1>
+)
+
+
+// function About(props){
+ 
+// return (
+//   <h1>About Page com</h1>
+// )
+// }
+
+export default About;
